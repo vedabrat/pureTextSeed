169a62354d53da3d837460fe26fe48ad83e467ad
vedabrat <vedabrat@gmail.com>
2018-09-27 21:02:59 -0400

    back to the old me

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'tranquil-mountain-51305.herokuapp.com';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
